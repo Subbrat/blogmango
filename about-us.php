18564c37fa9b40c1fb0fac4b48b01fe7a53d3c20
Subrat Mallick <m.subbrat@outlook.com>
2023-01-13 00:10:18 +0530

    asasd

--- about-us.php
+++ about-us.php
@@ -16,6 +16,7 @@ include_once('includes/config.php'); ?>
     <link rel="stylesheet" href="css/owl.carousel.css">
     <link rel="stylesheet" href="css/owl.theme.default.min.css">
     <!-- MAIN CSS -->
+    <link rel="stylesheet" href="./css/s6css.css">
     <link rel="stylesheet" href="css/style.css">
 </head>
 
--- includes/header.php
+++ includes/header.php
@@ -1,37 +1,43 @@
-     <!-- <section class="preloader">
+<head>
+    <link rel="stylesheet" href="./../css/s6css.css">
+</head>
+<!-- <section class="preloader">
          <div class="spinner">
              <span class="spinner-rotate"></span>
          </div>
      </section> -->
-     <section class="navbar custom-navbar navbar-fixed-top" role="navigation">
-         <div class="container">
-             <div class="navbar-header">
-                 <button class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                     <span class="icon icon-bar"></span>
-                     <span class="icon icon-bar"></span>
-                     <span class="icon icon-bar"></span>
-                 </button>
-                 <?php $query=mysqli_query($con,"select * from tblsite");
+<section class="navbar custom-navbar" role="navigation">
+    <div class="">
+        <div class="navbar-header">
+            <button class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="icon icon-bar"></span>
+                <span class="icon icon-bar"></span>
+                <span class="icon icon-bar"></span>
+            </button>
+            <?php $query=mysqli_query($con,"select * from tblsite");
 while($row=mysqli_fetch_array($query)){
 $logo=$row['siteLogo'];
 $wtitle=$row['siteTitle'];
 }  ?>
-                 <div align="center">
-                     <a href="index.php">
-                         <!-- lOGO TEXT HERE --><img src="admin/assets/images/<?php echo $logo;?>" width="50">
-                         <font style=" font-size: 20px; font-weight:bold; padding-left: 1%"><?php echo $wtitle;?></font>
-                     </a>
-                 </div>
-             </div>
-             <!-- MENU LINKS -->
-             <div class="collapse navbar-collapse">
-                 <ul class="nav navbar-nav navbar-nav-first">
-                     <li><a href="index.php">Home</a></li>
-                     <li><a href="blog-posts.php">Blog</a></li>
-                     <li><a href="about-us.php">About Us</a></li>
-                     <li><a href="contact.php">Contact Us</a></li>
-                     <li><a href="admin/">Admin Login</a></li>
-                 </ul>
-             </div>
-         </div>
-     </section>
+            <div align="s-center" class="s-hide-large ">
+                <a href="index.php">
+                    <img src="admin/assets/images/<?php echo $logo;?>" width="50">
+                </a>
+            </div>
+        </div>
+        <!-- MENU LINKS -->
+        <div class="collapse navbar-collapse">
+            <ul class="nav navbar-nav navbar-nav-first">
+                <li class="s-hide-small s-hide-medium"><a href="index.php"> <img
+                            src="admin/assets/images/<?php echo $logo;?>" width="50"></a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-nav-first s-right">
+                <li> <a href="index.php">Home</a></li>
+                <li><a href="blog-posts.php">Blog</a></li>
+                <li><a href="about-us.php">About Us</a></li>
+                <li><a href="contact.php">Contact Us</a></li>
+                <li><a href="admin/">Admin Login</a></li>
+            </ul>
+        </div>
+    </div>
+</section>
